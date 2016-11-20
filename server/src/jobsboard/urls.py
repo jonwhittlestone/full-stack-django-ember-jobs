b1c326963166575e32c5f959cc4077c1051751d5
Jon Whittlestone <jon@complyadvantage.com>
2016-11-20 15:59:25 +0000

    Better project name

--- server/src/jobsboard/urls.py
+++ server/src/jobsboard/urls.py
@@ -1,4 +1,4 @@
-"""jobs URL Configuration
+"""jobsboard URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
